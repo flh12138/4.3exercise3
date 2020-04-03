466afbfd775cfb5712d8716cf6a42c0d15433bf2
FLH0202 <FLH0202@163.com>
2020-04-03 14:37:29 +0800

    la

---
+++ 4.3exercise3/main.cpp
@@ -0,0 +1,29 @@
+#include<opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("E:\\PIC\\lena.jpg", 1);
+
+	if (srcMat.empty())return -1;
+
+	const cv::Point2f src_pt[] = {
+									cv::Point2f(200,200),
+									cv::Point2f(250,200),
+									cv::Point2f(200,100)};
+	const cv::Point2f dst_pt[] = {
+								   cv::Point2f(300,100),
+								   cv::Point2f(300,50),
+								   cv::Point2f(200,100) };
+
+	const cv::Mat affine_matrix = cv::getAffineTransform(src_pt,dst_pt);
+
+	cv::warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());
+
+	imshow("src1", srcMat);
+	imshow("src2", dstMat);
+	waitKey(0);
+}
